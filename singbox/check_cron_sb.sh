5e87de8a37a008880fe87d5b6c16eec4a876fafa
qlxi <98650473+qlxi@users.noreply.github.com>
2024-12-07 15:35:11 +0300

    Update check_cron_sb.sh

--- singbox/check_cron_sb.sh
+++ singbox/check_cron_sb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 SB_DIR="$HOME/.sb"
-#重命名 sing-box 程序名称
+# Переименование программы sing-box
 SB_EXE="sb-web"
 CRON_SB="nohup ${SB_DIR}/start.sh >/dev/null 2>&1 &"
-echo "检查并添加 crontab 任务"
+echo "Проверка и добавление задач в crontab"
 if [ -e "${SB_DIR}/start.sh" ]; then
-    echo "添加 sing-box 的 crontab 重启任务"
+    echo "Добавление задачи на перезагрузку для sing-box в crontab"
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_SB}") || (
         crontab -l
         echo "@reboot pkill -kill -u $(whoami) && ${CRON_SB}"
