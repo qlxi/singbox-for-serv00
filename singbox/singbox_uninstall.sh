aa013c547eef1ada7bd7cce1763b51f0e5d343d3
qlxi <98650473+qlxi@users.noreply.github.com>
2024-12-07 15:37:12 +0300

    Update singbox_uninstall.sh

--- singbox/singbox_uninstall.sh
+++ singbox/singbox_uninstall.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-#sing-box 安装路径
+# Путь установки sing-box
 SB_DIR="$HOME/.sb"
-#重命名 sing-box 程序名称
+# Переименование программы sing-box
 SB_EXE="sb-web"
 
 if [ -d "$SB_DIR" ]; then
-    read -p "是否确定卸载？（Y/N 回车N）: " choice
-    choice=${choice^^} # 转换为大写
+    read -p "Вы уверены, что хотите удалить? (Y/N, по умолчанию N): " choice
+    choice=${choice^^} # Преобразование в верхний регистр
     if [ "$choice" == "Y" ]; then
-        echo "卸载..."
-        # 在这里添加重置数据的代码
+        echo "Удаление..."
+        # Здесь добавьте код для сброса данных
     else
-        echo "已取消卸载..."
+        echo "Удаление отменено..."
         exit 1
     fi
 fi
-#停止程序运行
+# Остановка программы
 $SB_DIR/stop.sh
-#删除旧配置
+# Удаление старых настроек
 rm -rf $SB_DIR
-#删除相关定时任务（如果之前安装过）
+# Удаление связанных заданий (если были установлены ранее)
 crontab -l | grep -v $SB_EXE | crontab -
